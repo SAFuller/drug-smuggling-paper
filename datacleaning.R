dbc796f92b0d3c34dce4294d2d6e57faeda51931
SAFuller <46462337+SAFuller@users.noreply.github.com>
2019-01-08 10:28:04 -0600

    Update datacleaning.R
    Testing everything out

--- datacleaning.R
+++ datacleaning.R
@@ -10,3 +10,7 @@ gtdgroup<-aggregate(eventid~gname+iyear+imonth, data=gtd, FUN=length)
 
 #merge
 gtddrug<-merge(gtdgroup,drugdata, by.x="gname", by.y="GTD.name")
+
+#subset
+gtddrug2<-subset(gtddrug, select=c("gname","iyear","imonth","eventid","Network.Connections","Territory","Country","Drug1",
+                                   "Plant1","Plant2","Harvest1","Harvest2","Harvest3"))
